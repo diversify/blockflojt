71e053ecb59e2cdb8625379dd6533eb1f8a7590b
Sandra Liljeqvist <sandra.liljeqvist@gmail.com>
2014-11-22 15:05:12 +0100

    Added navbar and separated the app into three pages: main, admin and main

---
+++ app/scripts/controllers/admin.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name blockflojtApp.controller:AdminCtrl
+ * @description
+ * # AdminCtrl
+ * Controller of the blockflojtApp
+ */
+angular.module('blockflojtApp')
+  .controller('AdminCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
