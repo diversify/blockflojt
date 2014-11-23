4c9228e438bc246ba172311d3eec75a28d1ba921
Sandra Liljeqvist <sandra.liljeqvist@gmail.com>
2014-11-23 11:35:40 +0100

    Did so that you only can search for moods and styles that's supported by echo nest

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -15,9 +15,40 @@ angular.module('blockflojtApp')
     $scope.currentPhotoIndex = 0;
     $scope.hashtagArray;
 
+    var getPossibleValues = function(theType) {
+        var endPoint = 'http://developer.echonest.com/api/v4/artist/list_terms';
+        var paramsObj = {
+            api_key: 'CEWROMDFMFUUW1QCN',
+            type: theType,
+            callback: 'JSON_CALLBACK',
+            format: 'jsonp'
+        };
+        return $http.jsonp(endPoint, {params: paramsObj});
+    };
+
+    var possibleValues = {
+        mood: [],
+        style: []
+    };
+    var setPossibleValues = function() {
+        getPossibleValues('mood').then(function(response) {
+            var moodObj = response.data.response.terms;
+            for (var i in moodObj) {
+                possibleValues.mood.push(moodObj[i].name);
+            }
+        });
+
+        getPossibleValues('style').then(function(response) {
+            var styleObj = response.data.response.terms;
+            for (var i in styleObj) {
+                possibleValues.style.push(styleObj[i].name);
+            }
+        });
+    };
+
     var init = function(photoIndex) {
         if (!$rootScope.currentHashtag) {$rootScope.currentHashtag = 'happy';}
-        $scope.getPictures().then(function() {
+        getPictures().then(function() {
             var searchParameters,
                 hashtagIndex;
             $scope.hashtagArray = [];
@@ -33,13 +64,39 @@ angular.module('blockflojtApp')
                 } else {
                     searchParameters = angular.copy($scope.hashtagArray);
                 }
-                $scope.findSong(searchParameters);
+                findSong(searchParameters);
+                //findSong($scope.hashtagArray);
             }
         });
         //$timeout(init, 6000);
     };
 
-    $scope.findSong = function(searchParam) {
+    var getPictures = function() {
+        var InstagramClientID = '25e0c0b7ab2d47cbb2aad2589664aa93';
+        var head = 'https://api.instagram.com/v1/tags/';
+        var tail = '/media/recent?client_id=';
+        var callbackParam = '&callback=JSON_CALLBACK';
+        var requestURL = head + $scope.currentHashtag + tail + InstagramClientID + callbackParam;
+
+        return $http.jsonp(requestURL).
+            success(function(data, status, headers, config) {
+                var pic = data.data[0];
+                var picURL = pic.images.standard_resolution.url;
+                var user = pic.user;
+                var tags = pic.tags;
+
+                $scope.pictures.push({
+                    picURL: picURL,
+                    user: user,
+                    tags: tags
+                });
+            }).
+            error(function(data, status, headers, config) {
+                console.log('Error when fetching images from Instagram', data);
+            });
+    };
+
+    var findSong = function(searchParam) {
         var endPoint = 'http://developer.echonest.com/api/v4/song/search';
         var paramsPlaceholder = {
             api_key: 'CEWROMDFMFUUW1QCN',
@@ -49,14 +106,15 @@ angular.module('blockflojtApp')
             callback: 'JSON_CALLBACK',
             format: 'jsonp'
         };
-        var searchTypes = ['mood', 'artist', 'style'];
+        var searchTypes = ['mood', 'style', 'artist', 'title'];
         var promiseStuff = [];
         for (var k in searchParam) {
-            console.log('searchParam: '+searchParam[k]);
             for (var i in searchTypes) {
-                var paramsObj = angular.copy(paramsPlaceholder);
-                paramsObj[searchTypes[i]] = searchParam[k];
-                promiseStuff.push($http.jsonp(endPoint, {params: paramsObj}));
+                if (!possibleValues[searchTypes[i]] || possibleValues[searchTypes[i]].indexOf(searchParam[k]) > -1) {
+                    var paramsObj = angular.copy(paramsPlaceholder);
+                    paramsObj[searchTypes[i]] = searchParam[k];
+                    promiseStuff.push($http.jsonp(endPoint, {params: paramsObj}));
+                }
             }
         }
         $q.all(promiseStuff).then(function(response) {
@@ -75,36 +133,6 @@ angular.module('blockflojtApp')
                 init();
             }
         });
-    };
-
-
-    $scope.getPictures = function() {
-        var InstagramClientID = '25e0c0b7ab2d47cbb2aad2589664aa93';
-        var head = 'https://api.instagram.com/v1/tags/';
-        var tail = '/media/recent?client_id=';
-        var callbackParam = '&callback=JSON_CALLBACK';
-        var requestURL = head + $scope.currentHashtag + tail + InstagramClientID + callbackParam;
-
-        return $http.jsonp(requestURL).
-            success(function(data, status, headers, config) {
-                //$scope.pictures = [];
-
-                for(var i = 0; i < 20; i++) {
-                    var pic = data.data[i];
-                    var picURL = pic.images.standard_resolution.url;
-                    var user = pic.user;
-                    var tags = pic.tags;
-
-                    $scope.pictures.push({
-                        picURL: picURL,
-                        user: user,
-                        tags: tags
-                    });
-                }
-            }).
-            error(function(data, status, headers, config) {
-                console.log('Error when fetching images from Instagram', data);
-            });
     };
 
     init();
